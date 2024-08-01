2f2539e7c98489d77e0330e4a1da702cc2aa78bc
Lucas Takeshi <takeshioficial01@gmail.com>
2024-08-01 15:44:33 -0300

    style: adicionando estilização

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chillerlan QR Code generate PHP</title>
-    <link rel="stylesheet" type="text/css" href=""> <!-- css -->
+    <link rel="stylesheet" type="text/css" href="css/style.css"> <!-- css -->
   </head>
   <body>
-    
+    <div id="container">
+      <h2>Chillerlan QR Code PHP</h2>
+      <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
+        <div>
+          <input type="text" name="data" placeholder="Texto, URL ou outro tipo de dado">
+        </div>
+        <div>
+          <button type="submit">Generate</button>
+        </div>
+      </form>
+    </div>
   </body>
 </html>
