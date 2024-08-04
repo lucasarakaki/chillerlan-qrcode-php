99622b25a844e29bf74781685fa7803549a2fbac
Lucas Takeshi <takeshioficial01@gmail.com>
2024-08-04 15:53:11 -0300

    feat: composer install adicionando o installed

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '54cdd9d22ca609df4208303eefb2f2b8342ba869',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1a1fa5bb8864071327427856b45556840222fe6e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '54cdd9d22ca609df4208303eefb2f2b8342ba869',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1a1fa5bb8864071327427856b45556840222fe6e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
